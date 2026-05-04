a13cf7e4d4a537062cfc79cb6c738a408cdb0f3c
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2026-05-04 13:20:49 +0300

    Update Item.js

--- Item.js
+++ Item.js
@@ -35,6 +35,7 @@ router.get("/item/:name_of_the_category/:title", async (req, res) => {
 
     if (!categoryAttribute && title === "Coffee") {
       categoryAttribute = "coffee";
+      title="500 g Coffee Bag";
     }
     else if (!categoryAttribute && title!=="Coffee") {
       return res.status(400).json({ error: "Invalid category" });
